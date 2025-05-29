061eb4af2186a7017b8b29aff67575183d1d4cc8
mksoo <mgs3067@naver.com>
2025-05-29 16:54:10 +0900

    react query 도입

--- App.tsx
+++ App.tsx
@@ -10,16 +10,18 @@ import { NavigationContainer } from '@react-navigation/native';
 // import { createNativeStackNavigator } from '@react-navigation/native-stack'; // AppNavigator로 이동
 // import TodoListScreen from './src/screens/TodoListScreen'; // AppNavigator로 이동
 import AppNavigator from './src/navigation/AppNavigator'; // AppNavigator import
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // const Stack = createNativeStackNavigator(); // AppNavigator로 이동
+
+const queryClient = new QueryClient();
 
 function App(): React.JSX.Element {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
-    // <View>
-    //   <Text>Hello World</Text>
-    // </View>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
