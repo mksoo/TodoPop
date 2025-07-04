ddfd26da0c748790c882c75b3207b97088aff148
mksoo <mgs3067@naver.com>
2025-07-04 12:38:50 +0900

    util

---
+++ functions/src/utils/chunk.ts
@@ -0,0 +1,14 @@
+const chunk = <T>(array: T[], size: number): T[][] => {
+    if (size <= 0) {
+        return [];
+    }
+
+    const result: T[][] = [];
+    for (let i = 0; i < array.length; i += size) {
+        result.push(array.slice(i, i + size));
+    }
+
+    return result;
+}
+
+export default chunk;
