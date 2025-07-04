21cc95517fd54be969162e4e3bded1e629776d25
mksoo <mgs3067@naver.com>
2025-07-04 13:07:47 +0900

    noti doc 생성

--- functions/src/modules/notifications/notifications.service.ts
+++ functions/src/modules/notifications/notifications.service.ts
@@ -1,17 +1,19 @@
-import * as admin from 'firebase-admin';
+import * as admin from "firebase-admin";
 
-import chunk from '../../utils/chunk';
-import usersService from '../users/users.service';
-import { FieldValue } from 'firebase-admin/firestore';
-import sleep from '../../utils/sleep';
-import { Notification } from './notifications.interface';
-import { User } from '../users/users.interface';
-import { BaseMessage } from 'firebase-admin/lib/messaging/messaging-api';
+import chunk from "../../utils/chunk";
+import usersService from "../users/users.service";
+import { FieldValue } from "firebase-admin/firestore";
+import sleep from "../../utils/sleep";
+import { Notification } from "./notifications.interface";
+import { User } from "../users/users.interface";
+import { BaseMessage } from "firebase-admin/lib/messaging/messaging-api";
+import { ScheduleEntry } from "../scheduleEntries/scheduleEntries.interface";
+import dayjs from "dayjs";
 
 class NotificationService {
   private getCollectionRef(args: { uid: string }) {
     const { uid } = args;
-    return usersService.getRef({ id: uid }).collection('Notifications');
+    return usersService.getRef({ id: uid }).collection("Notifications");
   }
 
   private async addNotification(args: {
@@ -75,7 +77,7 @@ class NotificationService {
   }
 
   async sendMessage(args: {
-    receiver: Pick<User, 'fcmToken'>;
+    receiver: Pick<User, "fcmToken">;
     alarm: {
       title: string;
       body: string;
@@ -94,14 +96,54 @@ class NotificationService {
       return;
     }
     try {
-        await admin.messaging().send({
-            token: fcmToken,
-            ...this.generateFBBaseMessage({title, body, deepLink, imageUrl, inAppPush})
-        })
+      await admin.messaging().send({
+        token: fcmToken,
+        ...this.generateFBBaseMessage({title, body, deepLink, imageUrl, inAppPush})
+      })
     } catch (error) {
-        console.error(error);
-        throw error;
+      console.error(error);
+      throw error;
     }
+  }
+
+  formatDateWithKoreanDay(args: {date: Date}): string {
+    const {date} = args;
+    const dayjsDate = dayjs(date);
+    const days = ["일", "월", "화", "수", "목", "금", "토"];
+    const dayOfWeek = days[date.getDay()];
+
+    const formatted = dayjsDate.format(`YYYY.MM.DD(${dayOfWeek}) A hh:mm`);
+
+    return formatted;
+  }
+
+  // 일정 시간이 되면 알림을 보내는 함수
+  async notifySchedule(args: {
+    scheduleEntry: ScheduleEntry;
+  }) {
+    const {scheduleEntry} = args;
+    const {startAt, title, uid} = scheduleEntry;
+    const user = await usersService.findBy({id: uid});
+    if (!user) {
+      return;
+    }
+
+    if (!startAt) {
+      return;
+    }
+
+    // startAt을 yyyy.mm.dd(요일) 시간 형식으로 변환
+    const formattedStartAt = this.formatDateWithKoreanDay({date: startAt.toDate()});
+
+    await this.sendMessage({
+      receiver: {
+        fcmToken: user.fcmToken ?? "",
+      },
+      alarm: {
+        title,
+        body: formattedStartAt,
+      }
+    })
   }
 }
 
